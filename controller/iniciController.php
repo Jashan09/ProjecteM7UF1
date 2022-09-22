aa7889bad9d69acf96c4121357679b5e3d9799e2
daw25 <navjashan.singh@cirvianum.cat>
2022-09-22 16:16:00 +0200

    waiting

--- controller/iniciController.php
+++ controller/iniciController.php
@@ -5,25 +5,14 @@ if(session_start() === PHP_SESSION_NONE) session_start();
 $origen = $_POST["origen"];
 $destinacio = $_POST["destinacio"];
 
-$trajecte = checkTrajecte($origen, $destinacio);
+$_SESSION["trajecte"] = checkTrajecte($origen, $destinacio);
 
-
-if($trajecte=="12" or $trajecte == "21"){
-    header("Location: ../view/login.php");
+if($_SESSION["trajecte"] != null){
+    header("Location: ../view/ventaBitllets.php");
     exit();
 }
 
-if($trajecte=="13" or $trajecte == "31"){
-    header("Location: ../view/login.php");
-    exit();
-}
-
-if($trajecte=="23" or $trajecte == "32"){
-    header("Location: ../view/login.php");
-    exit();
-}
-
-if($trajecte==null){
+if($_SESSION["trajecte"]==null){
     echo "ERROR. L'origen i la destinació és la mateixa.";
     exit();
 }
