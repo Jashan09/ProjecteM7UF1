e4bd93fb1e19c03c09b9abf90405c230cada35d4
daw25 <navjashan.singh@cirvianum.cat>
2022-09-26 15:31:57 +0200

    tinc que solucionar el eliminar bitllet

--- view/viewsDelAdmin/gestionaBitllets.php
+++ view/viewsDelAdmin/gestionaBitllets.php
@@ -39,11 +39,8 @@ if(session_status() === PHP_SESSION_NONE) session_start();
                 <td><?php echo $bitllet->getDestinacio()?></td>
                 <td><?php echo $bitllet->getPreuDelBitllet()?></td>
                 <td><?php echo $bitllet->getPropietariDelBitllet()?></td>
-                <td>        <a href="../../controller/controllersDelAdmin/gestionaBitlletsController.php?idBitllet=<?php echo $bitllet->getIdBitllet(); ?>&operacio=editar " style="text-decoration: none; color:white; border-style: solid; color: black; background-color: cadetblue; padding: 0px 5px 0px 5px;"  >Editar</a>
-                </td>
-
-                <td>        <a href="../../controller/controllersDelAdmin/gestionaBitlletsController.php?idBitllet=<?php echo $bitllet->getIdBitllet(); ?>&operacio=eliminar " style="text-decoration: none; color:white; border-style: solid; color: black; background-color: indianred; padding: 0px 5px 0px 5px;"  >Eliminar</a>
-                </td>
+                <td><a href="../../controller/controllersDelAdmin/gestionaBitlletsController.php?idBitllet=<?php echo $bitllet->getIdBitllet(); ?>&operacio=editar " style="text-decoration: none; color:white; border-style: solid; color: black; background-color: cadetblue; padding: 0px 5px 0px 5px;"  >Editar</a></td>
+                <td><a href="../../controller/controllersDelAdmin/gestionaBitlletsController.php?idBitllet=<?php echo $bitllet->getIdBitllet(); ?>&operacio=eliminar " style="text-decoration: none; color:white; border-style: solid; color: black; background-color: indianred; padding: 0px 5px 0px 5px;"  >Eliminar</a></td>
             </tr>
             <?php
         }
